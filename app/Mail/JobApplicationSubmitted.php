e696db793a2e7dcc1923ac88a61a5d47c1a2c2fc
Allen Pittard <allenpittard@Allen-Pittard-MBP.local>
2020-11-23 12:17:08 -0500

    try to use view method

--- app/Mail/JobApplicationSubmitted.php
+++ app/Mail/JobApplicationSubmitted.php
@@ -31,43 +31,43 @@ class JobApplicationSubmitted extends Mailable
         $to = config('mail.to');
         $from = config('mail.from');
         $subject = 'Job Application Submitted';
-        $body = view('emails.job-application.submitted', [
-            'firstName'         => 'Donald',
-            'lastName'          => 'Pittard',
-            'address'           => '7712 Secretariat Dr',
-            'address2'          => '',
-            'city'              => 'Midlothian',
-            'state'             => 'VA',
-            'zip'               => '23112',
-            'phoneNumber'       => '757-450-1576',
-            'email'             => 'donaldpittard@gmail.com',
-            'position'          => 'Hotdog Salesman',
-            'hearAboutUs'       => 'Google',
-            'eligible'          => 'yes',
-            'salary'            => '$1000',
-            'startNow'          => 'no',
-            'startWhen'         => 'January 2, 2021',
-            'highSchoolName'    => 'Bishop Sullivan High School',
-            'highSchoolDiploma' => 'High School',
-            'collegeName'       => 'Old Dominion University',
-            'collegeMajor'      => 'Computer Science',
-            'collegeDegree'     => 'Bachelor of Science',
-            'collegeYears'      => '3',
-            'otherEdName'       => '',
-            'otherEdMajor'      => '',
-            'otherEdDegree'     => '',
-            'otherEdYears'      => '',
-            'otherCourseWork'   => '',
-            'employment1Name'   => 'Synalloy',
-            'employment1Start'  => 'March 03, 2018',
-            'employment1Stop'   => 'March 03, 2020',
-            'employment1Duties' => '',
-            'employment1Reason' => '' 
-         ])->render();
 
         return $this
             ->from($from)
             ->subject($subject)
-            ->setBody($body);
+            ->view('emails.job-application.submitted')
+            ->with([
+                'firstName'         => 'Donald',
+                'lastName'          => 'Pittard',
+                'address'           => '7712 Secretariat Dr',
+                'address2'          => '',
+                'city'              => 'Midlothian',
+                'state'             => 'VA',
+                'zip'               => '23112',
+                'phoneNumber'       => '757-450-1576',
+                'email'             => 'donaldpittard@gmail.com',
+                'position'          => 'Hotdog Salesman',
+                'hearAboutUs'       => 'Google',
+                'eligible'          => 'yes',
+                'salary'            => '$1000',
+                'startNow'          => 'no',
+                'startWhen'         => 'January 2, 2021',
+                'highSchoolName'    => 'Bishop Sullivan High School',
+                'highSchoolDiploma' => 'High School',
+                'collegeName'       => 'Old Dominion University',
+                'collegeMajor'      => 'Computer Science',
+                'collegeDegree'     => 'Bachelor of Science',
+                'collegeYears'      => '3',
+                'otherEdName'       => '',
+                'otherEdMajor'      => '',
+                'otherEdDegree'     => '',
+                'otherEdYears'      => '',
+                'otherCourseWork'   => '',
+                'employment1Name'   => 'Synalloy',
+                'employment1Start'  => 'March 03, 2018',
+                'employment1Stop'   => 'March 03, 2020',
+                'employment1Duties' => '',
+                'employment1Reason' => '' 
+             ]);
     }
 }
