ac33635d4c75587a05008b516ed62dd77711ecdd
Allen Pittard <allenpittard@Allen-Pittard-MBP.local>
2020-11-23 11:31:39 -0500

    Create mailable

--- app/Http/Controllers/ReviewJobApplicationController.php
+++ app/Http/Controllers/ReviewJobApplicationController.php
@@ -2,51 +2,16 @@
 
 namespace App\Http\Controllers;
 
+use App\Mail\JobApplicationSubmitted;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Mail;
 use PHPMailer\PHPMailer\PHPMailer;
 
 class ReviewJobApplicationController extends Controller
 {
     public function __invoke(Request $request)
     {
-        $to      = 'donaldpittard@gmail.com';
-        $subject = 'Career Investigations (Donald)';
-        $body    = view('review-job-application', [
-            'firstName'         => 'Donald',
-            'lastName'          => 'Pittard',
-            'address'           => '7712 Secretariat Dr',
-            'address2'          => '',
-            'city'              => 'Midlothian',
-            'state'             => 'VA',
-            'zip'               => '23112',
-            'phoneNumber'       => '757-450-1576',
-            'email'             => 'donaldpittard@gmail.com',
-            'position'          => 'Hotdog Salesman',
-            'hearAboutUs'       => 'Google',
-            'eligible'          => 'yes',
-            'salary'            => '$1000',
-            'startNow'          => 'no',
-            'startWhen'         => 'January 2, 2021',
-            'highSchoolName'    => 'Bishop Sullivan High School',
-            'highSchoolDiploma' => 'High School',
-            'collegeName'       => 'Old Dominion University',
-            'collegeMajor'      => 'Computer Science',
-            'collegeDegree'     => 'Bachelor of Science',
-            'collegeYears'      => '3',
-            'otherEdName'       => '',
-            'otherEdMajor'      => '',
-            'otherEdDegree'     => '',
-            'otherEdYears'      => '',
-            'otherCourseWork'   => '',
-            'employment1Name'   => 'Synalloy',
-            'employment1Start'  => 'March 03, 2018',
-            'employment1Stop'   => 'March 03, 2020',
-            'employment1Duties' => '',
-            'employment1Reason' => '' 
-         ])->render();
-   
-        mail($to, $subject, $body);
-        
-        return $body;
+        return Mail::to(config('app.mail.to'))
+            ->send(new JobApplicationSubmitted());
     }
 }
---
+++ app/Mail/JobApplicationSubmitted.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace App\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Mail\Mailable;
+use Illuminate\Queue\SerializesModels;
+
+class JobApplicationSubmitted extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    /**
+     * Create a new message instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Build the message.
+     *
+     * @return $this
+     */
+    public function build()
+    {
+        $to = config('app.mail.to');
+        $from = config('app.mail.from');
+        $subject = 'Job Application Submitted';
+        $body = view('review-job-application', [
+            'firstName'         => 'Donald',
+            'lastName'          => 'Pittard',
+            'address'           => '7712 Secretariat Dr',
+            'address2'          => '',
+            'city'              => 'Midlothian',
+            'state'             => 'VA',
+            'zip'               => '23112',
+            'phoneNumber'       => '757-450-1576',
+            'email'             => 'donaldpittard@gmail.com',
+            'position'          => 'Hotdog Salesman',
+            'hearAboutUs'       => 'Google',
+            'eligible'          => 'yes',
+            'salary'            => '$1000',
+            'startNow'          => 'no',
+            'startWhen'         => 'January 2, 2021',
+            'highSchoolName'    => 'Bishop Sullivan High School',
+            'highSchoolDiploma' => 'High School',
+            'collegeName'       => 'Old Dominion University',
+            'collegeMajor'      => 'Computer Science',
+            'collegeDegree'     => 'Bachelor of Science',
+            'collegeYears'      => '3',
+            'otherEdName'       => '',
+            'otherEdMajor'      => '',
+            'otherEdDegree'     => '',
+            'otherEdYears'      => '',
+            'otherCourseWork'   => '',
+            'employment1Name'   => 'Synalloy',
+            'employment1Start'  => 'March 03, 2018',
+            'employment1Stop'   => 'March 03, 2020',
+            'employment1Duties' => '',
+            'employment1Reason' => '' 
+         ])->render();
+
+        return $this
+            ->from($from)
+            ->subject($subject)
+            ->view($body);
+    }
+}
